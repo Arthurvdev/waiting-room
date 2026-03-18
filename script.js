8246e20d8ab17c862f1df5e9b92327cbfb7c6011
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2026-03-18 17:58:33 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-/*document.addEventListener("click", () => {
-    const main = document.querySelector('.audio_main');
-    if (main) main.play();
-}, { once: true });*/
+const man = document.querySelector('.man');
+const man_sound = document.querySelector('.man_sound');
+const song_photos = document.querySelector('.song_photos');
+const close_photo1 = document.querySelector('#close_photo1');
+
+man.addEventListener("mouseover", () => {
+    man_sound.loop = true; 
+    man_sound.play();
+});
+
+man.addEventListener("mouseleave", () => {
+    man_sound.loop = false; 
+    man_sound.pause();
+    man_sound.currentTime = 0; 
+});
+
+man.addEventListener("click", () => {
+    song_photos.loop = true;
+    song_photos.play();
+});
+
+close_photo1.addEventListener("click", () => {
+    song_photos.loop = false;
+    song_photos.pause();
+    song_photos.currentTime = 0; 
+});
+
+const box_img = document.querySelector('.box');
+const box_sound = document.querySelector('.box_sound');
+
+box_img.addEventListener("mouseover", () => {
+    box_sound.loop = true;
+    box_sound.play();
+});
+
+box_img.addEventListener("mouseleave", () => {
+    box_sound.loop = false;
+    box_sound.pause();
+    box_sound.currentTime = 0;
+});
 
 function escada() {
     const black = document.querySelector('.black');
     const main = document.querySelector('.main');
     const passos = document.querySelector('.passos');
     const escada = document.querySelector('.escada');
-    console.log("click");
 
     black.style.zIndex = '10';
     black.style.opacity = '100%';
@@ -54,4 +89,17 @@ function waiting() {
 function box() {
     alert("Você não pode abrir a caixa antes da hora... \n\n You can't open the box before the time...");
     alert("O que pode ter aqui dentro? Qual mistério ou aleatoriedade bizarra te espera? \n\n What could be inside? What mystery or bizarre randomness awaits you?");
+}
+
+function photo1() {
+    console.log("Clicou na foto 1");
+    const div_photo = document.querySelector('.photo_1');
+    div_photo.style.marginTop = "0vh";
+
+}
+
+function subir_photo1() {
+    const div_photo = document.querySelector('.photo_1');
+    div_photo.style.marginTop = "-100vh";
+    // Opcional: adicionar transição suave no CSS para não ser instantâneo
 }
