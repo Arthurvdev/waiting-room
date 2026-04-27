5f7973569b1fe27bb5803a614763ee6a110ab10c
Arthur Vinícius M. <99282591+Arthurvdev@users.noreply.github.com>
2026-04-27 00:11:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,6 +7,49 @@ const seta2 = document.querySelector('.seta2');
 const old_room = document.querySelector('.old_room');
 const nostalgic_song = document.querySelector('.nostalgic_song');
 const aip = document.querySelector('.aip');
+const box2 = document.querySelector('.box');
+const teleport_sound_effect = document.querySelector('.teleport_sound');
+const begin_div = document.querySelector('.BEGIN');
+const container_flutuar = document.querySelector('.flutuar');
+const waiting2 = document.querySelector('.waiting');
+
+// BOX CONTENT
+
+const link1 = document.querySelector('#link1');
+const link2 = document.querySelector('#link2');
+const link3 = document.querySelector('#link3');
+const link4 = document.querySelector('#link4');
+const link5 = document.querySelector('#link5');
+
+link1.addEventListener("click", () => {
+    window.location.href = "link1.html";
+});
+
+link2.addEventListener("click", () => {
+    window.location.href = "link2.html";
+});
+
+link3.addEventListener("click", () => {
+    window.location.href = "link3.html";
+});
+
+link4.addEventListener("click", () => {
+    window.location.href = "link4.html";
+});
+
+link5.addEventListener("click", () => {
+    window.location.href = "link5.html";
+});
+
+box2.addEventListener("click", () => {
+    teleport_sound_effect.play();
+    begin_div.style.zIndex = '15';
+    begin_div.style.opacity = '100%';
+    container_flutuar.borderRadius = '0%';
+    //begin_div.style.height = '100vh';
+    //begin_div.style.width = '100vw';
+        
+});
 
 seta.addEventListener("click", () => {
     nostalgic_song.loop = true;
@@ -100,7 +143,7 @@ function waiting() {
         black.style.zIndex = '-1';
         black.style.opacity = '0%';
         passos.pause();
-        aip.play();
+        //aip.play();
     }, 2000);
 }
 
